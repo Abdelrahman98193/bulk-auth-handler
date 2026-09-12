55c322384ea22ee7ceb2785e76561fd5337217c3
Abdelrahman Saber <69966590+Abdelrahman98193@users.noreply.github.com>
2026-09-12 15:41:00 +0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,6 @@
 export default function handler(req, res) {
   const clientId = "Ov23liI43WU1kZ9OSaEy";
-  const host = req.headers.host;
-  const redirectUri = `https://${host}/api/callback`;
+  const redirectUri = "https://bulk-auth-handler-7mw584uxe-tas25.vercel.app/api/callback";
 
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo,user`;
 
